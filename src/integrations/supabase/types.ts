68867ca4de32f7903f2d5e94cf6b21a41707a6d0
softgenai <admin@softgen.ai>
2025-12-21 13:10:44 +0000

    fix(users): Delete 'Admin Sistema' user

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -12,4 +12,4 @@ export type Enums<T extends keyof Database['public']['Enums']> = Database['publi
 export type Functions<T extends keyof Database['public']['Functions']> = Database['public']['Functions'][T];
 
 // Project reference: ngeltlygytupgdjiagve
-// Last updated: 2025-12-21T11:56:56.857758
+// Last updated: 2025-12-21T13:10:15.499238
