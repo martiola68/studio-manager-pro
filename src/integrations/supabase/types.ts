e00d6386f0d882dab6abb1de9f922d9b45fb7c64
martiola68 <martiola68@users.noreply.github.com>
2026-02-10 14:14:04 +0000

    feat(auth): Add Microsoft 365 configuration variables

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -12,4 +12,4 @@ export type Enums<T extends keyof Database['public']['Enums']> = Database['publi
 export type Functions<T extends keyof Database['public']['Functions']> = Database['public']['Functions'][T];
 
 // Project reference: ngeltlygytupgdjiagve
-// Last updated: 2026-02-10T13:34:46.418998
+// Last updated: 2026-02-10T13:57:41.567050
