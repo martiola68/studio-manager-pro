b24e189e9aeb32fa7d357b536ca932a9917f9821
martiola68 <martiola68@tiscali.it>
2026-03-17 11:56:54 +0100

    Update modello-av4.tsx

--- src/pages/antiriciclaggio/modello-av4.tsx
+++ src/pages/antiriciclaggio/modello-av4.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/router";
 import { getSupabaseClient } from "@/lib/supabaseClient";
-import TitolariDaRappLegaliForm from "@/components/antiriciclaggio/TitolariDaRappLegaliForm";
+import TitolariEffettiviForm from "@/components/antiriciclaggio/TitolariEffettiviForm";
+import TitolariDaRappLegaliForm from "@/pages/antiriciclaggio/TitolariDaRappLegaliForm";
 
 type ClienteOption = {
   id: string;
