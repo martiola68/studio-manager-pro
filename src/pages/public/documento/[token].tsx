51ebe61f8cbb6e8929c95ee539c2fc066594cc05
martiola68 <martiola68@tiscali.it>
2026-04-01 18:05:03 +0200

    Update [token].tsx

--- src/pages/public/documento/[token].tsx
+++ src/pages/public/documento/[token].tsx
@@ -87,6 +87,12 @@ const MAX_FILE_SIZE_MB = 10;
 const MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024;
 
 export default function PublicDocumentoPage() {
+
+  useEffect(() => {
+  const supabase = getSupabaseClient()
+  supabase.auth.signOut()
+}, [])
+  
   const router = useRouter();
 
   const token = useMemo(
