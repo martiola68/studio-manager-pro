45c3f21693c36d9736c32d36d30f931254d9554c
martiola68 <martiola68@tiscali.it>
2026-04-06 19:43:02 +0200

    Update modello-av1.tsx

--- src/pages/antiriciclaggio/modello-av1.tsx
+++ src/pages/antiriciclaggio/modello-av1.tsx
@@ -932,7 +932,7 @@ const handlePrestazioneChange = (prestazioneValue: string) => {
 
           {isTB1 && (
             <p className="mt-2 text-sm font-medium text-red-700">
-              Sezione B non obbligatoria per prestazioni con Tipo TB = TB1.
+              Sezione B non obbligatoria per prestazioni con Tipo prestazione rientrante nella tabella 1
             </p>
           )}
         </div>
