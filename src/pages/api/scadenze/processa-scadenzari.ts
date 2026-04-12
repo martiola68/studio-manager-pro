4df989bf0019d099286a7b13b7625fe8615df78c
martiola68 <martiola68@tiscali.it>
2026-04-12 20:05:30 +0200

    Update processa-scadenzari.ts

--- src/pages/api/scadenze/processa-scadenzari.ts
+++ src/pages/api/scadenze/processa-scadenzari.ts
@@ -5,12 +5,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "POST") {
-    return res.status(405).json({
-      success: false,
-      error: "Metodo non consentito",
-    });
-  }
+ if (req.method !== "POST" && req.method !== "GET") {
+  return res.status(405).json({
+    success: false,
+    error: "Metodo non consentito",
+  });
+}
 
   try {
     const { forceAlert1, forceAlert2 } = req.body || {};
