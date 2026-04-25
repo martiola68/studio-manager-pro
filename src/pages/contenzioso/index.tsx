0a87d277b5c3cb3b2bf74f3bae761c80a3afdbbe
martiola68 <martiola68@tiscali.it>
2026-04-25 16:21:06 +0200

    Update index.tsx

--- src/pages/contenzioso/index.tsx
+++ src/pages/contenzioso/index.tsx
@@ -180,12 +180,21 @@ export default function ContenziosoIndexPage() {
           </p>
         </div>
 
-        <Link href="/contenzioso/avvisi-bonari/nuovo">
-          <Button className="gap-2">
-            <Plus className="h-4 w-4" />
-            Nuovo avviso bonario
-          </Button>
-        </Link>
+<div className="flex gap-2">
+  <Link href="/contenzioso/tipi-atto">
+    <Button variant="outline">
+      <Plus className="mr-2 h-4 w-4" />
+      Nuovo atto
+    </Button>
+  </Link>
+
+  <Link href="/contenzioso/avvisi-bonari/nuovo">
+    <Button>
+      <Plus className="mr-2 h-4 w-4" />
+      Nuovo avviso bonario
+    </Button>
+  </Link>
+</div>
       </div>
 
       <Card className="mb-6">
