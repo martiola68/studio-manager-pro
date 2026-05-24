32ba41933e8be4b686642f70f774fffbd47bcd0d
martiola68 <martiola68@tiscali.it>
2026-05-24 11:51:21 +0200

    Update [id].tsx

--- src/pages/pratiche/[id].tsx
+++ src/pages/pratiche/[id].tsx
@@ -546,9 +546,6 @@ if (pratica?.tipo?.classe_form === "cambio_amministratore") {
   return <FormCambioAmministratore pratica={pratica} />;
 }
 
-return <div>Classe form non gestita</div>;
-}
-
 const thStyle: React.CSSProperties = {
   textAlign: "left",
   padding: "12px 16px",
