b5f32502cfabfcd0b1f0089c5b32f9b4bcfdeb77
martiola68 <martiola68@tiscali.it>
2026-05-28 15:09:09 +0200

    Update nuovo.tsx

--- src/pages/controllo-gestione/nuovo.tsx
+++ src/pages/controllo-gestione/nuovo.tsx
@@ -20,7 +20,9 @@ export default function NuovoControlloGestione() {
   });
 
   useEffect(() => {
-    fetch("/api/clienti").then((r) => r.json()).then(setClienti);
+   fetch("/api/controllo-gestione/clienti-disponibili")
+  .then((r) => r.json())
+  .then(setClienti);
     fetch("/api/utenti").then((r) => r.json()).then(setUtenti);
   }, []);
 
