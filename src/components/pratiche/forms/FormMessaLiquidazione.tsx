fb1f0e5279b3d2be2c067fe30cadf13c5f480a2e
martiola68 <martiola68@tiscali.it>
2026-05-31 11:26:50 +0200

    Update FormMessaLiquidazione.tsx

--- src/components/pratiche/forms/FormMessaLiquidazione.tsx
+++ src/components/pratiche/forms/FormMessaLiquidazione.tsx
@@ -1074,7 +1074,7 @@ function aggiornaCampo(campo: string, valore: string) {
           }
 
 
- const res = await fetch("/api/rapp-legali/save", {
+ const res = await fetch(`/api/rapp-legali/${praticaId}/nuovo-rapp-pratiche`, {
             method: "POST",
  body: JSON.stringify({
   studio_id: pratica?.studio_id || pratica?.cliente?.studio_id,
