8a60253e95e374a62e29c437d6df46e5a192d478
martiola68 <martiola68@tiscali.it>
2026-06-02 19:41:16 +0200

    Create scadenzari-config.ts

---
+++ src/lib/scadenzari-config.ts
@@ -0,0 +1,76 @@
+export const SCADENZARI_CONFIG = {
+  "imu:acconto": {
+    table: "tbscadimu",
+    flag: "conferma_acconto_imu",
+  },
+
+  "imu:saldo": {
+    table: "tbscadimu",
+    flag: "conferma_saldo_imu",
+  },
+
+  "imu:dichiarazione": {
+    table: "tbscadimu",
+    flag: "conferma_dichiarazione_imu",
+  },
+
+  "lipe:1_trimestre": {
+    table: "tbscadlipe",
+    flag: "conferma_1_trimestre",
+  },
+
+  "lipe:2_trimestre": {
+    table: "tbscadlipe",
+    flag: "conferma_2_trimestre",
+  },
+
+  "lipe:3_trimestre": {
+    table: "tbscadlipe",
+    flag: "conferma_3_trimestre",
+  },
+
+  "lipe:4_trimestre": {
+    table: "tbscadlipe",
+    flag: "conferma_4_trimestre",
+  },
+
+  "iva:acconto": {
+    table: "tbscadlipe",
+    flag: "conferma_acconto_iva",
+  },
+
+  "fiscale:ires_saldo_acconto": {
+    table: "tbscadfiscali",
+    flag: "conferma_ires_saldo_acconto",
+  },
+
+  "fiscale:cciaa": {
+    table: "tbscadfiscali",
+    flag: "conferma_cciaa",
+  },
+
+  "fiscale:ires_secondo_acconto": {
+    table: "tbscadfiscali",
+    flag: "conferma_ires_secondo_acconto",
+  },
+
+  "fiscale:invio_dichiarazione": {
+    table: "tbscadfiscali",
+    flag: "conferma_invio_dichiarazione",
+  },
+
+  "fiscale:irap_saldo_acconto": {
+    table: "tbscadfiscali",
+    flag: "conferma_irap_saldo_acconto",
+  },
+
+  "fiscale:irap_secondo_acconto": {
+    table: "tbscadfiscali",
+    flag: "conferma_irap_secondo_acconto",
+  },
+
+  "fiscale:irap_invio_dichiarazione": {
+    table: "tbscadfiscali",
+    flag: "conferma_irap_invio_dichiarazione",
+  },
+};
