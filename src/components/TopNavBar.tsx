7a683f04e10a17face3cc8db208ba1aa2600b613
martiola68 <martiola68@tiscali.it>
2026-06-25 09:25:18 +0200

    Update TopNavBar.tsx

--- src/components/TopNavBar.tsx
+++ src/components/TopNavBar.tsx
@@ -388,12 +388,7 @@ void loadEventiImminenti();
       href: "/pratiche",
       icon: <FolderKanban className="h-4 w-4" />,
     },
-    {
-  label: "Nuova pratica",
-  href: "/pratiche/nuova",
-  icon: <FileText className="h-4 w-4" />,
-},
-{
+   {
   label: "Variazioni CCIAA / AdE",
   href: "/pratiche/variazioni",
   icon: <FileText className="h-4 w-4" />,
