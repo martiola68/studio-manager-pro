c2a9b6f341b6aba2269faee1f0c11f7ff9f8be76
martiola68 <martiola68@tiscali.it>
2026-07-08 11:19:21 +0200

    Update index.ts

--- src/pages/api/payroll/richieste-area-cliente/index.ts
+++ src/pages/api/payroll/richieste-area-cliente/index.ts
@@ -71,7 +71,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   return res.status(200).json({
     success: true,
     richiesta: data,
-    allegati: allegati || [],
   });
 }
 
